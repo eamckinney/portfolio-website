1e6bb3e4d7b03be4d17de75fdaa6768b42830bbb
Emily McKinney <emily.a.mckinney@gmail.com>
2020-03-29 21:28:30 -0500

    Fixed font-family inheritance

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.css';
 import 'bootstrap-social/bootstrap-social.css';
 import 'typeface-lora';
+import App from './App';
+
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
