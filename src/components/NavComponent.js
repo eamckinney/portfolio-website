be882baf7756d5c53439a5fa8a24884e65525313
Emily McKinney <emily.a.mckinney@gmail.com>
2020-04-11 08:48:02 -0500

    Improved Navbar, targeting & scrolling

--- src/components/NavComponent.js
+++ src/components/NavComponent.js
@@ -1,50 +1,72 @@
-import React, { Component }  from 'react';
-import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem } from 'reactstrap';
-import { NavLink } from 'react-router-dom';
+import React, { Component } from "react";
+import {
+	Nav,
+	Navbar,
+	NavbarBrand,
+	NavbarToggler,
+	Collapse,
+	NavItem,
+} from "reactstrap";
+import { NavLink } from "react-router-dom";
+//importing zenscroll makes all scrolling smooth...
+import { zenscroll } from "zenscroll";
 
 class Navigation extends Component {
-    constructor(props) {
-        super(props);
+	constructor(props) {
+		super(props);
 
-        this.state = {
-          isNavOpen: false
-        };
-    }
+		this.state = {
+			isNavOpen: false,
+		};
+	}
 
-    toggleNav = () => {
-        this.setState({
-            //when toggled, flip to either true or false
-            isNavOpen: !this.state.isNavOpen
-        });
-    }
+	toggleNav = () => {
+		this.setState({
+			//when toggled, flip to either true or false
+			isNavOpen: !this.state.isNavOpen,
+		});
+	};
 
-    render() {
-        return (
-            
-            <Navbar dark sticky="top" expand="md">
-                <NavbarToggler onClick={this.toggleNav} />
-                <Collapse isOpen={this.state.isNavOpen} navbar>
-                    <Nav navbar className="nav-center">
-                        <NavItem>
-                            <NavLink className="nav-link underline" to="/home"> Home </NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink className="nav-link underline" to="/about"> About </NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink className="nav-link underline" to="/projects"> Projects </NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink className="nav-link underline" to="/connect"> Connect </NavLink>
-                        </NavItem>         
-                    </Nav>
-                </Collapse>
-                <NavbarBrand className="site-logo mx-auto" href="#Home"><span className="logo-text">EM</span></NavbarBrand>
-            </Navbar>
-            
-        );
-    }    
-
+	render() {
+		return (
+			<Navbar dark sticky="top" expand="md">
+				<NavbarToggler onClick={this.toggleNav} />
+				<Collapse isOpen={this.state.isNavOpen} navbar>
+					<Nav navbar className="nav-center">
+						<ul class="navbar-nav mx-auto">
+							<li class="nav-item border-right text-center">
+								<a class="nav-link underline" href="#Home">
+									{" "}
+									<span> Home </span>
+								</a>
+							</li>
+							<li class="nav-item border-right text-center">
+								<a class="nav-link underline" href="#About">
+									{" "}
+									About{" "}
+								</a>
+							</li>
+							<li class="nav-item border-right text-center">
+								<a class="nav-link underline" href="#Projects">
+									{" "}
+									Projects{" "}
+								</a>
+							</li>
+							<li class="nav-item text-center">
+								<a class="nav-link underline" href="#Connect">
+									{" "}
+									Connect{" "}
+								</a>
+							</li>
+						</ul>
+					</Nav>
+				</Collapse>
+				<NavbarBrand className="site-logo mx-auto" href="#Home">
+					<span className="logo-text">EM</span>
+				</NavbarBrand>
+			</Navbar>
+		);
+	}
 }
 
 export default Navigation;
