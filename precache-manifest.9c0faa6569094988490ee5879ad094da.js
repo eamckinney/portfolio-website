bb191aef9c36a0e522af5d8a5f5f9c59ec5cf4a2
Emily McKinney <emily.a.mckinney@gmail.com>
2020-12-06 15:11:12 -0600

    Updates

---
+++ precache-manifest.9c0faa6569094988490ee5879ad094da.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e67d4f1264ba3fd7e84241fe340d2d1",
+    "url": "/portfolio-website/index.html"
+  },
+  {
+    "revision": "f2c431c6480f8cfc21fa",
+    "url": "/portfolio-website/static/css/2.7068e68c.chunk.css"
+  },
+  {
+    "revision": "0d4ecc9a885766e0bd08",
+    "url": "/portfolio-website/static/css/main.fbe45b27.chunk.css"
+  },
+  {
+    "revision": "f2c431c6480f8cfc21fa",
+    "url": "/portfolio-website/static/js/2.1f24e280.chunk.js"
+  },
+  {
+    "revision": "2089965cc3e0fbb1dda2d650d8277df4",
+    "url": "/portfolio-website/static/js/2.1f24e280.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d4ecc9a885766e0bd08",
+    "url": "/portfolio-website/static/js/main.dadf4485.chunk.js"
+  },
+  {
+    "revision": "1a00bad0967666d6b1ad",
+    "url": "/portfolio-website/static/js/runtime-main.871ec804.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/portfolio-website/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/portfolio-website/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/portfolio-website/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/portfolio-website/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/portfolio-website/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/portfolio-website/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portfolio-website/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/portfolio-website/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portfolio-website/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portfolio-website/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portfolio-website/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/portfolio-website/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/portfolio-website/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/portfolio-website/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/portfolio-website/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/portfolio-website/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "0d78d370987954fb6b9f0efec3065e83",
+    "url": "/portfolio-website/static/media/lora-latin-400.0d78d370.woff"
+  },
+  {
+    "revision": "e4cdb14bf148f2846997a6be7ba648bd",
+    "url": "/portfolio-website/static/media/lora-latin-400.e4cdb14b.woff2"
+  },
+  {
+    "revision": "2c4801fad2634e6dac678d8826cf417c",
+    "url": "/portfolio-website/static/media/lora-latin-400italic.2c4801fa.woff2"
+  },
+  {
+    "revision": "7beffbacbbde86423a7ee771f31c1626",
+    "url": "/portfolio-website/static/media/lora-latin-400italic.7beffbac.woff"
+  },
+  {
+    "revision": "1617380e0dea667b61cf44e86f3d0f10",
+    "url": "/portfolio-website/static/media/lora-latin-700.1617380e.woff"
+  },
+  {
+    "revision": "ce18d17335e3ef2119d76f5dff177c66",
+    "url": "/portfolio-website/static/media/lora-latin-700.ce18d173.woff2"
+  },
+  {
+    "revision": "6ec37b950cf9829a2cad7d02b11810c9",
+    "url": "/portfolio-website/static/media/lora-latin-700italic.6ec37b95.woff"
+  },
+  {
+    "revision": "b4bb1fa2335d49d4bc7e7ddd944cee44",
+    "url": "/portfolio-website/static/media/lora-latin-700italic.b4bb1fa2.woff2"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/portfolio-website/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/portfolio-website/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/portfolio-website/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/portfolio-website/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/portfolio-website/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
