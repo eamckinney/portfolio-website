b142eacb83a0fc2bd158f53ae6486fa108bc0e1e
Emily McKinney <emily.a.mckinney@gmail.com>
2022-02-07 16:54:49 -0600

    logbook up & running (and working)

--- src/components/HundredDaysComponent.js
+++ src/components/HundredDaysComponent.js
@@ -1,16 +1,73 @@
 import React, { Component, useState } from "react";
 import { Form, Label, Divider, Rating, Header } from "semantic-ui-react";
+import { ENTRIES } from '../shared/logbookEntries';
 import { Link } from "react-router-dom";
 import { Button } from "reactstrap";
 import "../App.css";
 import "../HundredDays.css";
 
+
 export default function HundredDays() {
+
+  const[entries]=useState(ENTRIES);  
+  const[selectedEntry, setSelectedEntry]=useState([entries[entries.length-1]]);
+
+  // Sort so that most recent is first
+  entries.sort((a, b) => (a.id < b.id) ? 1 : -1);
+
+  const entryTitles = entries.map(entry => {
+    return(
+      <div>
+        <div className="row" onClick={() => setSelectedEntry([entry])}>
+          <div className="col text-right logTitle" tabIndex={entry.id}>
+            <h3> { entry.day } </h3>
+            <p><i> { entry.title } </i></p>
+          </div>
+        </div>
+        <Divider />
+      </div>
+    );
+  });
+
+  const entryText = selectedEntry.map(entry => {
+    
+    const resources = entry.resources.map(resource => {
+      return(
+        <li><a href={resource} target="_blank">{resource}</a></li>
+      );
+    });
+
+    return(
+      <div className="row mt-3">
+        <div className="col logEntry">
+          <span style={{"letterSpacing": "3px", "fontFamily":"Avenir", "fontSize":".8em"}}>{entry.day.toUpperCase()}</span>
+          <h4> {entry.title} </h4>
+          <p className="logPara">
+            <span><b>Date:</b> {entry.date}</span><br/>
+            <span><b>Language/Tools:</b> {entry.tools} </span><br/>
+            <span><b>URL:</b> <a href={entry.url} target="_blank">{entry.url}</a></span><br/>
+          </p>
+          <p className="logPara">
+            <b>What I did / what I learned: </b>
+            {entry.learned}
+          </p>
+          <span className="logPara">
+            <b>Additional resources used: </b>
+            <ul style={{"lineHeight": "1.3"}}>
+              {resources}
+            </ul>
+          </span>
+          
+          
+        </div>
+      </div>
+    );
+  })
 
   return(
 
     <div className="hundredDaysPage" id="Top">
-				<div className="container mb-4 pl-0">
+				<div className="container mb-5 pl-0 text-center">
 					<div className="row">
 						<div className="col">
 							<h2 className="appTitle mb-0">Logbook</h2>
@@ -22,7 +79,7 @@ export default function HundredDays() {
 					<div className="row">
 						<div className="col">
 							<Link to="/home" className="text-center">
-								<Button className="btn-outline-dark">Back to Home</Button>
+								<Button className="btn-outline-dark" style={{"fontSize": ".8em"}}>Back to Home</Button>
 							</Link>
 						</div>
 					</div>
@@ -33,45 +90,11 @@ export default function HundredDays() {
 					<div className="row">
             
             <div className="col-md-4 logCol">
-              <div className="row">
-                <div className="col text-right logTitle">
-                  <h3>Day 1</h3>
-                  <p><i>Leetcode challenge "Two Sum"</i></p>
-                </div>
-              </div>
-              <Divider />
-              <div className="row">
-                <div className="col text-right logTitle">
-                  <h3>Day 2</h3>
-                  <p><i>Create 100 Days of Code logbook on portfolio website</i></p>
-                </div>
-              </div>
+              {entryTitles}
             </div>
 
             <div className="col-md-8 entryCol"> 
-              <div className="row">
-                <div className="col logEntry">
-                  <span style={{"letter-spacing": "3px"}}>DAY 1</span>
-                  <h4>Leetcode challenge 'Two Sum'</h4>
-                  <p style={{"font-size": ".8em"}}>
-                    <span><b>Date:</b> Thursday, February 3, 2022</span><br/>
-                    <span><b>Language/Tools:</b> JavaScript</span><br/>
-                    <span><b>URL:</b> https://leetcode.com/problems/two-sum/submissions/</span><br/>
-                  </p>
-                  <p>
-                    <b>What I did / what I learned: </b>
-                    What a hashmap is! It's basically an object/dictionary that is faster to search than a typical array. Converting your array to a Map() object and using the Map.has() and Map.get() methods are quite a bit faster than using the similar Array.indexOf() method. There is more to learn here.
-                  </p>
-                  <p style={{"font-size": ".8em"}}>
-                    <b>Additional resources used: </b>
-                    <ul>
-                      <li>https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map</li>
-                      <li>https://www.cloudhadoop.com/2020/03/javascript-examples-convert-array-of.html</li>
-                      <li>https://www.callicoder.com/two-sum-problem/</li>
-                    </ul>
-                  </p>
-                </div>
-              </div>
+              {entryText}
             </div>
           </div>
         </div>
