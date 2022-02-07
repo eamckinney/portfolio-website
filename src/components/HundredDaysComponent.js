07b945a823c048b22eb1dfcf6be748eb70163592
Emily McKinney <emily.a.mckinney@gmail.com>
2022-02-07 13:13:17 -0600

    finish basic logbook layout

--- src/components/HundredDaysComponent.js
+++ src/components/HundredDaysComponent.js
@@ -27,25 +27,51 @@ export default function HundredDays() {
 						</div>
 					</div>
 				</div>
+
+
         <div className="container appStyle">
 					<div className="row">
+            
             <div className="col-md-4 logCol">
               <div className="row">
-                <div className="col text-right">
+                <div className="col text-right logTitle">
                   <h3>Day 1</h3>
-                  <p>Leetcode challenge "Two Sum"</p>
-                  <Divider />
+                  <p><i>Leetcode challenge "Two Sum"</i></p>
                 </div>
               </div>
-              
+              <Divider />
               <div className="row">
-                <div className="col text-right">
-                  <h3>Day 1</h3>
-                  <p>Leetcode challenge "Two Sum"</p>
+                <div className="col text-right logTitle">
+                  <h3>Day 2</h3>
+                  <p><i>Create 100 Days of Code logbook on portfolio website</i></p>
                 </div>
               </div>
             </div>
+
             <div className="col-md-8 entryCol"> 
+              <div className="row">
+                <div className="col logEntry">
+                  <span style={{"letter-spacing": "3px"}}>DAY 1</span>
+                  <h4>Leetcode challenge 'Two Sum'</h4>
+                  <p style={{"font-size": ".8em"}}>
+                    <span><b>Date:</b> Thursday, February 3, 2022</span><br/>
+                    <span><b>Language/Tools:</b> JavaScript</span><br/>
+                    <span><b>URL:</b> https://leetcode.com/problems/two-sum/submissions/</span><br/>
+                  </p>
+                  <p>
+                    <b>What I did / what I learned: </b>
+                    What a hashmap is! It's basically an object/dictionary that is faster to search than a typical array. Converting your array to a Map() object and using the Map.has() and Map.get() methods are quite a bit faster than using the similar Array.indexOf() method. There is more to learn here.
+                  </p>
+                  <p style={{"font-size": ".8em"}}>
+                    <b>Additional resources used: </b>
+                    <ul>
+                      <li>https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map</li>
+                      <li>https://www.cloudhadoop.com/2020/03/javascript-examples-convert-array-of.html</li>
+                      <li>https://www.callicoder.com/two-sum-problem/</li>
+                    </ul>
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
